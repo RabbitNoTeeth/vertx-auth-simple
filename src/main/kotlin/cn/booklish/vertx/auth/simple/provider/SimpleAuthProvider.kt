254952eed3cfd47766f054f3cad728602586fd46
don9 <864426487@qq.com>
2018-02-07 15:40:32 +0800

    去掉kotlin

--- src/main/kotlin/cn/booklish/vertx/auth/simple/provider/SimpleAuthProvider.kt
+++
@@ -1,6 +0,0 @@
-package cn.booklish.vertx.auth.simple.provider
-
-import io.vertx.ext.auth.AuthProvider
-
-
-interface SimpleAuthProvider:AuthProvider
--- src/main/kotlin/cn/booklish/vertx/auth/simple/util/SimpleUtil.kt
+++
@@ -1,5 +0,0 @@
-package cn.booklish.vertx.auth.simple.util
-
-import java.util.*
-
-fun getUuid():String = UUID.randomUUID().toString().replace("-","")

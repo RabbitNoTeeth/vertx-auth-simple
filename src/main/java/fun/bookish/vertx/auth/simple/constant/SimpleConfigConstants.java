755393b93d5286278a0e5c868e2e793b98544684
liuxindong <864426487@qq.com>
2018-02-13 20:44:33 +0800

    添加对自定义配置的支持

---
+++ src/main/java/fun/bookish/vertx/auth/simple/constant/SimpleConfigConstants.java
@@ -0,0 +1,16 @@
+package fun.bookish.vertx.auth.simple.constant;
+
+/**
+ * 常量
+ */
+public class SimpleConfigConstants {
+
+    private SimpleConfigConstants(){}
+
+    public static final String SESSION_TIMEOUT = "sessionTimeout";
+
+    public static final String REMEMBERME_TIMEOUT = "rememberMeTimeout";
+
+    public static final String AES_KEY = "aesKey";
+
+}
